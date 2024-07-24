b96ec8c5337bc4bfa5d5e0e20eab9e339e4cbbf1
Jose Feliciano <janaya@greenapplic.com>
2024-07-24 01:16:06 -0500

    feat: Creación de las rutas directas
    - Creación del component input personalizado
    - Seteo de las rutas y pruebas
    - Creación del formulario y seteo de la ruta
    - Traslado del listado productos a su propio componente

---
+++ src/app/ui/home/components/input-custom/input-custom.component.ts
@@ -0,0 +1,16 @@
+import { Component, input } from '@angular/core';
+import { Input } from '@angular/core';
+@Component({
+  selector: 'input-custom',
+  standalone: true,
+  imports: [],
+  templateUrl: './input-custom.component.html',
+  styleUrl: './input-custom.component.css',
+})
+export class InputCustomComponent {
+  @Input() name: string = '';
+  @Input() placeholder: string = '';
+  @Input() style: string = '';
+  @Input() label: string = '';
+  @Input() isDisable: boolean = false;
+}
